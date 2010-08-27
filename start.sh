86051d5d5a0fc33f0b31414f6f3a500f4b97788b
Yusuke Hata <nowelium+github@gmail.com>
2010-08-27 18:18:48 +0900

    update 0.06

--- start.sh
+++ start.sh
@@ -34,7 +34,7 @@ export _JAVA_OPTIONS=$JAVA_OPTIONS
 DIR=$(cd $(dirname $0);pwd)
 
 LIB=$DIR
-LIB=$LIB:"$DIR/dist/temperance-0.05.jar"
+LIB=$LIB:"$DIR/dist/temperance-0.06.jar"
 LIB=$LIB:"$DIR/resources"
 LIB=$LIB:"$DIR/lib/libmecab-0.01.jar"
 LIB=$LIB:"$DIR/lib/libmemcached-0.01.jar"
